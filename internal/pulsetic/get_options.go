fe1bf4ccbccfc90c45998eabfe3e0d2ca805645e
Gabe Cook <gabe565@gmail.com>
2025-04-19 15:53:08 -0500

    fix(pulsetic): fix incorrect monitor match if no filter inputs are set

--- internal/pulsetic/get_options.go
+++ internal/pulsetic/get_options.go
@@ -1,24 +1,24 @@
 package pulsetic
 
 type FindRequest struct {
-	URL string
-	ID  int64
+	URL *string
+	ID  *int64
 }
 
 func (r FindRequest) Matches(m Monitor) bool {
-	return r.URL == "" || r.URL == m.URL && r.ID == 0 || r.ID == m.ID
+	return (r.URL != nil && *r.URL == m.URL) || (r.ID != nil && *r.ID == m.ID)
 }
 
 type FindOption func(*FindRequest)
 
 func FindByURL(url string) FindOption {
 	return func(r *FindRequest) {
-		r.URL = url
+		r.URL = &url
 	}
 }
 
 func FindByID(id int64) FindOption {
 	return func(r *FindRequest) {
-		r.ID = id
+		r.ID = &id
 	}
 }
